314ca0b080307969ea247eb115ff7b3ce0526245
Isaac <isaac.kennedy@weclever.co>
2021-06-05 23:24:07 -0300

    Add ENV variables

--- src/shared/infra/mongodb/database/index.ts
+++ src/shared/infra/mongodb/database/index.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-mongoose.connect('mongodb://localhost:27017/uolcompasso', { useNewUrlParser: true, useUnifiedTopology: true });
+dotenv.config();
+
+mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'database connection error:'));
